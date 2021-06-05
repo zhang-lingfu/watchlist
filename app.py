aa6f511a3586b995345d96c338f98f7d9c54f3a0
unknown <1516770361@qq.com>
2021-06-05 18:58:10 +0800

    Add database support with Falsk-SQLAlchemy

--- app.py
+++ app.py
@@ -76,3 +76,8 @@ def initdb(drop):
         db.drop_all()
     db.create_all()
     click.echo('Initialized database.')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    user = User.query.first()
+    return render_template('404.html',user=user)
